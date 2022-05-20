f1297fbf543e5ca0fa27db0a4a135b41e0526b29
Perk <natspindola@hotmail.com>
2022-05-20 20:09:03 -0300

    validando uma model

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,13 +17,33 @@ namespace MinhaDemoMVC.Controllers
         [Route("pagina-inicial/{id:int}/{categoria?}")] //rota da action por atributo - é obrigado ter um ID mas não uma categoria
         public IActionResult Index(int id, string categoria)
         {
-            return View();
+            var filme = new Filme()
+            {
+                Titulo = "Teste",
+                DataLancamento = DateTime.Now,
+                Genero = null,
+                Avaliacao = 3,
+                Valor = 100000
+            };
+
+            return RedirectToAction("Privacy", filme);
+            //return View();
         }
 
         [Route("privacidade")] //rota da action
         [Route("politica-de-privacidade")] //as duas funcionam, mas a última vai ser usada como padrão
-        public IActionResult Privacy()
+        public IActionResult Privacy(Filme filme)
         {
+            if(ModelState.IsValid)
+            {
+
+            }
+
+            foreach(var error in ModelState.Values.SelectMany(M => M.Errors))
+            {
+                Console.WriteLine(error.ErrorMessage);
+            }
+
             return View();
 
             //var fileBytes = System.IO.File.ReadAllBytes(@"C:\arquivo.txt");
