381b0125dbd2e4e31cc84c7ab54d94f52f77181d
Perk <natspindola@hotmail.com>
2022-05-24 19:44:43 -0300

    scaffolding

--- Startup.cs
+++ Startup.cs
@@ -9,6 +9,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MinhaDemoMVC.Data;
 
 namespace MinhaDemoMVC
 {
@@ -33,6 +35,9 @@ namespace MinhaDemoMVC
 
 
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<MinhaDemoMVCContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("MinhaDemoMVCContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
